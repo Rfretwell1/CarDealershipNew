64acb4cb5db2f6911815eec82ab029d4e15df32b
p16200157 <p16200157@GWHXX582WIN008W>
2018-03-07 13:18:43 +0000

    shaq

--- CarClassLibrary/clsPayment.cs
+++ CarClassLibrary/clsPayment.cs
@@ -10,6 +10,29 @@ namespace CarClassLibrary
         public string Description { get; set; }
         public string EmployeeID { get; set; }
         public string FirstName { get; set; }
+        public int PaymentNo
+        {
+            get
+            {
+                return mPaymentNo;
+            }
+            set
+            {
+                mPaymentNo = value; 
+            }
+        }
         public string SurnameName { get; set; }
+
+        public bool Find(Int32 PaymentID)
+        {
+            //set the private data member to the test data value
+            mPaymentNo = 21;
+            //always return true 
+            return true;
+        }
+        //private data member to the test data value 
+        private Int32 mPaymentNo;
+
     }
+    
 }
--- Testing/tstPayment.cs
+++ Testing/tstPayment.cs
@@ -18,6 +18,19 @@ namespace tstPayment
         }
 
         [TestMethod]
+        public void PaymentNoPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsPayment AnPayment = new clsPayment();
+            //create some test data to assign to the property 
+            int TestData = 1;
+            //assign the data to the property 
+            AnPayment.PaymentNo = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnPayment.PaymentNo, TestData);
+        }
+
+        [TestMethod]
          public void CarIDPropertyOK()
         {
             //create an instance of the class we want to create 
@@ -109,6 +122,45 @@ namespace tstPayment
             Assert.AreEqual(AnPayment.SurnameName, TestData);
         }
 
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create 
+            clsPayment AnPayment = new clsPayment();
+            //boolean variable to store the result of the validation 
+            Boolean Found = false;
+            //create some test data to use with method
+            Int32 PaymentNo = 21;
+            //invoke the mothod 
+            Found = AnPayment.Find(PaymentNo);
+            //test to see that the result is correct
+            Assert.IsTrue(Found);
+
+
+        }
+
+        [TestMethod]
+        public void TestEmployeeNoFound()
+        {
+            //create an instance of the class we want to create 
+            clsPayment AnPayment = new clsPayment();
+            //boolean variable to store the result of the validation 
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is) 
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 PaymentNo = 21;
+            //string EmployeeID = "E12345";
+            //invoke the method 
+            Found = AnPayment.Find(PaymentNo);
+            //check the address no 
+            if (AnPayment.PaymentNo != 21)
+            {
+                OK = false;
+            }
+            //test to see that the result is correctr 
+            Assert.IsTrue(OK);
+        }
 
 
     }
