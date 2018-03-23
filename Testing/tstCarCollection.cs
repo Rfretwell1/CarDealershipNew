11eb407ba04105066275c5ab4616b88d76c0d110
p16215514 <p16215514@GWHXX682WIN104W>
2018-03-23 13:04:43 +0000

    Commit

--- Testing/tstCarCollection.cs
+++ Testing/tstCarCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CarClassLibrary;
+using CarTest;
 
 namespace Testing
 {
@@ -15,7 +16,27 @@ namespace Testing
             //test to see that it exists
             Assert.IsNotNull(AllCars);
         }
+        [TestMethod]
+        public void ThisCarPropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsCarCollection AllAddresses = new clsCarCollection();
+            //create some test data to assign to the property
+            clsCar TestCar = new clsCar();
+            //set the properties of the test object
+            TestCar.Active = true;
+            TestCar.CarMake = "Honda";
+            TestCar.CarModel = "Civic";
+            TestCar.Colour = "red";
+            TestCar.Age = 5;
+            TestCar.BodyType = "Sedan";
+            TestCar.Mileage = 100000;
+            //assign the data to the property
+            AllAddresses.ThisCar = TestCar;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllAddresses.ThisCar, TestCar);
 
+        }
 
     }
 }
